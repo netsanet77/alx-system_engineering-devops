5176ca3afde2d0edd2e19a604a11cbf35b8d9a86
netsanet <netsanetbikes@gmail.com>
2023-01-11 00:42:48 -0800

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file in /tmp
-file { '/tmp':
-path => '/tmp/school',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+
+file { '/tmp/school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
 ~                           
